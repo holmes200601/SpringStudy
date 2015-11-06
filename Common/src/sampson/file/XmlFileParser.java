f9079fa41c924f2f01df4ef97153d7fc948d7296
Sampson He <sampson.he@sap.com>
2015-11-06 18:34:11 +0800

    Continue testing framework

--- Common/src/sampson/file/XmlFileParser.java
+++ Common/src/sampson/file/XmlFileParser.java
@@ -1,10 +1,17 @@
 package sampson.file;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
 public class XmlFileParser {
     private String xmlFileName = null;
+    private Document parsedDom = null;
     
     public XmlFileParser(String xmlFileName) {
         this.xmlFileName = xmlFileName;
+        DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
     }
     
     public static String getElementValueById(Long id) {
--- TestEntrance/src/sampson/main/MainEntrance.java
+++ TestEntrance/src/sampson/main/MainEntrance.java
@@ -1,6 +1,13 @@
 package sampson.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import sampson.file.XmlFileParser;
 
 public class MainEntrance {
     public static void main(String[] args) {
@@ -11,11 +18,21 @@ public class MainEntrance {
          * 3) Read tester file for each tester bean
          * 4) Execute each tester and print log
          * */
-        
+        String configFile = "config.xml";
+        String testerFile = "tester.xml";
         try(AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class)) {
-            
+            List<Class<?>> configClassList = MainEntrance.readConfigClass(configFile);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    private static List<Class<?>> readConfigClass(String configFile) {
+        List<Class<?>> resultList = new ArrayList<Class<?>>();
+        
+        
+        
+        
+        return resultList;
     }
 }
