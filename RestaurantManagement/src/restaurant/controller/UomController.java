c3ec5fd1eb4d517a30b733495313c19eb33938b3
Sampson He <sampson.he@sap.com>
2016-05-09 17:52:55 +0800

    Add patch for uom

--- RestaurantManagement/src/restaurant/controller/UomController.java
+++ RestaurantManagement/src/restaurant/controller/UomController.java
@@ -13,49 +13,58 @@ import restaurant.uom.bean.entity.UomGroup;
 import restaurant.utils.ReflectionUtilsPlus;
 
 @RestController
-@RequestMapping(value="/uom")
+@RequestMapping(value = "/uom")
 public class UomController extends ControllerBase {
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public UomRO getUom(@PathVariable Long id) {
-		Uom uom = getBeanFacade().loadBean(Uom.class, id, true);
-		UomRO result = getDozerMapper().map(uom, UomRO.class);
-		
-		return result;
-	}
-	
-	@RequestMapping(value="/group/{id}", method=RequestMethod.GET)
-	public UomGroupRO getUomGroup(@PathVariable Long id) {
-		UomGroup loadedGroup = getBeanFacade().loadBean(UomGroup.class, id, true);
-		UomGroupRO result = getDozerMapper().map(loadedGroup, UomGroupRO.class);
-		
-		return result;
-	}
-	
-	@RequestMapping(value="", method=RequestMethod.POST)
-	public Long createUom(@RequestBody UomRO uom) {
-		Uom uomEntity = getDozerMapper().map(uom, Uom.class);
-		Long result = getBeanFacade().saveBean(uomEntity);
-		
-		return result;
-	}
-	
-	@RequestMapping(value="/group", method = RequestMethod.POST)
-	public Long createUomGroup(@RequestBody UomGroupRO uomGroup) {
-		UomGroup groupEntity = getDozerMapper().map(uomGroup, UomGroup.class);
-		Long result = getBeanFacade().saveBean(groupEntity);
-		
-		return result;
-	}
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public UomRO getUom(@PathVariable Long id) {
+        Uom uom = getBeanFacade().loadBean(Uom.class, id, true);
+        UomRO result = getDozerMapper().map(uom, UomRO.class);
 
-	@Override
-	@RequestMapping(value="/template", method = RequestMethod.GET)
-	public Object getInstance() {
-		// TODO Auto-generated method stub
-		return ReflectionUtilsPlus.newInstance(UomRO.class);
-	}
-	
-	@RequestMapping(value="/group/template", method = RequestMethod.GET)
-	public Object getInstanceOne() {
-		return ReflectionUtilsPlus.newInstance(UomGroupRO.class);
-	}
+        return result;
+    }
+
+    @RequestMapping(value = "/group/{id}", method = RequestMethod.GET)
+    public UomGroupRO getUomGroup(@PathVariable Long id) {
+        UomGroup loadedGroup = getBeanFacade().loadBean(UomGroup.class, id, true);
+        UomGroupRO result = getDozerMapper().map(loadedGroup, UomGroupRO.class);
+
+        return result;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Long createUom(@RequestBody UomRO uom) {
+        Uom uomEntity = getDozerMapper().map(uom, Uom.class);
+        Long result = getBeanFacade().saveBean(uomEntity);
+
+        return result;
+    }
+
+    @RequestMapping(value = "/group", method = RequestMethod.POST)
+    public Long createUomGroup(@RequestBody UomGroupRO uomGroup) {
+        UomGroup groupEntity = getDozerMapper().map(uomGroup, UomGroup.class);
+        Long result = getBeanFacade().saveBean(groupEntity);
+
+        return result;
+    }
+
+    @RequestMapping(value = "/group/{id}", method = RequestMethod.PATCH)
+    public Long udpateUomGroup(@RequestBody UomGroupRO uomGroup, @PathVariable Long id) {
+        UomGroup group = getBeanFacade().loadBean(UomGroup.class, id, true);
+        getDozerMapper().map(uomGroup, group);
+        getBeanFacade().saveOrUpdate(group);
+
+        return group.getId();
+    }
+
+    @Override
+    @RequestMapping(value = "/template", method = RequestMethod.GET)
+    public Object getInstance() {
+        // TODO Auto-generated method stub
+        return ReflectionUtilsPlus.newInstance(UomRO.class);
+    }
+
+    @RequestMapping(value = "/group/template", method = RequestMethod.GET)
+    public Object getInstanceOne() {
+        return ReflectionUtilsPlus.newInstance(UomGroupRO.class);
+    }
 }
